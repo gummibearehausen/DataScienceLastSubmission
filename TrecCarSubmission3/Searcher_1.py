7a725c082d0f58f63cc6f507bf63766d1a13acfc
Bahram Behzadian <zodiac_se@yahoo.com>
2017-04-23 18:33:17 -0400

    interface

--- TrecCarSubmission3/Searcher_1.py
+++ TrecCarSubmission3/Searcher_1.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 INDEX_DIR = "BaseIndexFolder"
 # INDEX_DIR = "IndexFiles.index"
@@ -13,15 +14,7 @@ from org.apache.lucene.store import SimpleFSDirectory
 from org.apache.lucene.search import IndexSearcher
 
 
-"""
-This script is loosely based on the Lucene (java implementation) demo class
-org.apache.lucene.demo.SearchFiles.  It will prompt for a search query, then it
-will search the Lucene index in the current directory called 'index' for the
-search query entered against the 'contents' field.  It will then display the
-'path' and 'name' fields for each of the hits it finds in the index.  Note that
-search.close() is currently commented out because it causes a stack overflow in
-some cases.
-"""
+
 def run(searcher, analyzer):
     while True:
         print ()
@@ -42,7 +35,20 @@ def run(searcher, analyzer):
             print ('id:', doc.get("paraId"))
 
 
-def run1(searcher, analyzer, queries, hits_per_query, output_file, model):
+def text_normalisation(scoreDoc, doc_facet, searcher):
+    lucene_doc = searcher.doc(scoreDoc.doc)
+    para_text = lucene_doc.get(doc_facet)
+    # print(para_text)
+    normalised_text = re.sub(r"[^A-Za-z0-9]", " ", para_text)
+    return normalised_text
+
+
+def top_k_pseudo_feedback(scoreDocs, top_k, doc_facet, searcher):
+    pseudo_fk = [text_normalisation(s, doc_facet, searcher) for s in scoreDocs[:top_k]]
+    return pseudo_fk
+
+
+def run1(searcher, analyzer, queries, hits_per_query, output_file):
     #queries = [ [query_text],[query_id]]
     queries_text = queries[0]
     queries_ids = queries[1]
@@ -57,38 +63,8 @@ def run1(searcher, analyzer, queries, hits_per_query, output_file, model):
         query = QueryParser("contents", analyzer).parse(query_as_text)
         scoreDocs = searcher.search(query, hits_per_query).scoreDocs
         print ("%s total matching documents." % len(scoreDocs))
-
-
-
-        if int(model) == 2:
-            top_k = 5
-            def text_normalisation(scoreDoc):
-                lucene_doc = searcher.doc(scoreDoc.doc)
-                para_text = lucene_doc.get("contents")
-                normalised_text = re.sub(r"[^A-Za-z0-9]"," ",para_text)
-
-                return normalised_text
-            pseudo_feedback = [ text_normalisation(s) for s in scoreDocs[:top_k]]
-            pseudo_feedback_as_text = " ".join(pseudo_feedback)
-            query_prime = QueryParser("contents", analyzer).parse(pseudo_feedback_as_text)
-
-            scoreDocs_prime = searcher.search(query_prime, hits_per_query).scoreDocs
-            scoreDocs = scoreDocs_prime
-
         runfile_writer(scoreDocs, searcher, output_file, query_as_id)
 
-        # for scoreDoc in scoreDocs:
-        #     doc = searcher.doc(scoreDoc.doc)
-        #     # print ('id:', doc.get("paraId"), 'Contents', doc.get("contents"))
-        #     print ('id:', doc.get("paraId"))
-        #     search_result_query = ' '.join([query_as_id,str(0),
-        #                                     doc.get("paraId"),
-        #                                     str(rank),
-        #                                     str(1.0/rank),
-        #                                     "BBT"])
-        #     # print(search_result_query)
-        #     output_file.write(search_result_query+"\n")
-        #     rank += 1
 
 
 def runfile_writer(scoreDocs, searcher, output_file, query_as_id):
@@ -114,7 +90,7 @@ def runfile_writer(scoreDocs, searcher, output_file, query_as_id):
         output_file.write(search_result_query + "\n")
 
 
-def search_engine_1(queries, hits, model):
+def search_engine_1(queries, hits):
     run_file = codecs.open("runfile", "w", "utf-8")
     # lucene.initVM(vmargs=['-Djava.awt.headless=true'])
     print ('lucene', lucene.VERSION)
@@ -122,9 +98,11 @@ def search_engine_1(queries, hits, model):
     directory = SimpleFSDirectory(Paths.get(os.path.join(base_dir, INDEX_DIR)))
     searcher = IndexSearcher(DirectoryReader.open(directory))
     analyzer = StandardAnalyzer()
-    run1(searcher, analyzer, queries, hits, run_file, model)
+    run1(searcher, analyzer, queries, hits, run_file)
     del searcher
     run_file.close()
+
+
 
 
 def search_engine_general():
