69c9c2b87c05fa0b06b10eddcb3c1927127ae5f1
Hang Zhang <bryan.zhanghang@gmail.com>
2017-04-23 15:32:17 -0400

    add models, para meter to the interface indicating different models

--- TrecCarSubmission3/Searcher_1.py
+++ TrecCarSubmission3/Searcher_1.py
@@ -42,7 +42,7 @@ def run(searcher, analyzer):
             print ('id:', doc.get("paraId"))
 
 
-def run1(searcher, analyzer, queries, hits_per_query, output_file):
+def run1(searcher, analyzer, queries, hits_per_query, output_file, model):
     #queries = [ [query_text],[query_id]]
     queries_text = queries[0]
     queries_ids = queries[1]
@@ -58,22 +58,63 @@ def run1(searcher, analyzer, queries, hits_per_query, output_file):
         scoreDocs = searcher.search(query, hits_per_query).scoreDocs
         print ("%s total matching documents." % len(scoreDocs))
 
-        rank=1
+
+
+        if int(model) == 2:
+            top_k = 5
+            def text_normalisation(scoreDoc):
+                lucene_doc = searcher.doc(scoreDoc.doc)
+                para_text = lucene_doc.get("contents")
+                normalised_text = re.sub(r"[^A-Za-z0-9]"," ",para_text)
+
+                return normalised_text
+            pseudo_feedback = [ text_normalisation(s) for s in scoreDocs[:top_k]]
+            pseudo_feedback_as_text = " ".join(pseudo_feedback)
+            query_prime = QueryParser("contents", analyzer).parse(pseudo_feedback_as_text)
+
+            scoreDocs_prime = searcher.search(query_prime, hits_per_query).scoreDocs
+            scoreDocs = scoreDocs_prime
+
+        runfile_writer(scoreDocs, searcher, output_file, query_as_id)
+
+        # for scoreDoc in scoreDocs:
+        #     doc = searcher.doc(scoreDoc.doc)
+        #     # print ('id:', doc.get("paraId"), 'Contents', doc.get("contents"))
+        #     print ('id:', doc.get("paraId"))
+        #     search_result_query = ' '.join([query_as_id,str(0),
+        #                                     doc.get("paraId"),
+        #                                     str(rank),
+        #                                     str(1.0/rank),
+        #                                     "BBT"])
+        #     # print(search_result_query)
+        #     output_file.write(search_result_query+"\n")
+        #     rank += 1
+
+
+def runfile_writer(scoreDocs, searcher, output_file, query_as_id):
+    rank = 1
+    if len(scoreDocs)!=0:
         for scoreDoc in scoreDocs:
             doc = searcher.doc(scoreDoc.doc)
-            # print ('id:', doc.get("paraId"), 'Contents', doc.get("contents"))
             print ('id:', doc.get("paraId"))
-            search_result_query = ' '.join([query_as_id,str(0),
+            search_result_query = ' '.join([query_as_id, str(0),
                                             doc.get("paraId"),
                                             str(rank),
-                                            str(1.0/rank),
+                                            str(1.0 / rank),
                                             "BBT"])
             # print(search_result_query)
-            output_file.write(search_result_query+"\n")
+            output_file.write(search_result_query + "\n")
             rank += 1
+    else:
+        search_result_query = ' '.join([query_as_id, str(0),
+                                        "none",
+                                        str(rank),
+                                        str(1.0 / rank),
+                                        "BBT"])
+        output_file.write(search_result_query + "\n")
 
 
-def search_engine_1(queries, hits):
+def search_engine_1(queries, hits, model):
     run_file = codecs.open("runfile", "w", "utf-8")
     # lucene.initVM(vmargs=['-Djava.awt.headless=true'])
     print ('lucene', lucene.VERSION)
@@ -81,7 +122,7 @@ def search_engine_1(queries, hits):
     directory = SimpleFSDirectory(Paths.get(os.path.join(base_dir, INDEX_DIR)))
     searcher = IndexSearcher(DirectoryReader.open(directory))
     analyzer = StandardAnalyzer()
-    run1(searcher, analyzer, queries, hits, run_file)
+    run1(searcher, analyzer, queries, hits, run_file, model)
     del searcher
     run_file.close()
 
--- TrecCarSubmission3/System_interface.py
+++ TrecCarSubmission3/System_interface.py
@@ -9,6 +9,8 @@ from Evaluation import eval_result
 arg[1]=paragraphs
 arg[2]=outline
 arg[3]=hirarchical_qrel
+arg[4]: 1->baseline model tfidf
+        2->top k pseudeo feedback as query model
 paras are indexed in the folder 'BaseIndexFolder'
 """
 
@@ -21,6 +23,8 @@ os.path.join(base_dir, INDEX_DIR)
 indexParas = IndexFiles(sys.argv[1],os.path.join(base_dir, INDEX_DIR))
 queries=read_outline(sys.argv[2])
 hits_per_query = 2000
-search_engine_1(queries, hits_per_query)
+
+model = sys.argv[4]
+search_engine_1(queries, hits_per_query, model)
 eval_result(sys.argv[3])
 
